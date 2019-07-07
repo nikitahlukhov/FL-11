0fc3b33228492503ac05d19d32f8d345148a006d
Nikita Hlukhov <zholud92@gmail.com>
2019-07-08 00:16:40 +0300

    hw6

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,13 +1,13 @@
-let a = Number(prompt("A"));
-let b = Number(prompt("B"));
-let c = Number(prompt("C"));
+let a = Number(prompt('A'));
+let b = Number(prompt('B'));
+let c = Number(prompt('C'));
 
 if (a + b < c || b + c < a || c + a < b) {
   console.log("Triangle doesn't exist");
 } else if (a === b && c === a) {
-  console.log("Eequivalent triangle");
+  console.log('Eequivalent triangle');
 } else if (a === b || b === c || c === a) {
-  console.log("Isosceles triangle");
+  console.log('Isosceles triangle');
 } else {
-  console.log("Normal triangle");
+  console.log('Normal triangle');
 }
