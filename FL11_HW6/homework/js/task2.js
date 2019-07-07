9d48b9cf2b8fc65ed474a1191778286c5555fa96
Nikita Hlukhov <zholud92@gmail.com>
2019-07-08 00:20:43 +0300

    hw6

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -3,7 +3,7 @@ let b = Number(prompt('B'));
 let c = Number(prompt('C'));
 
 if (a + b < c || b + c < a || c + a < b) {
-  console.log("Triangle doesn't exist");
+  console.log('Triangle doesn\'t exist');
 } else if (a === b && c === a) {
   console.log('Eequivalent triangle');
 } else if (a === b || b === c || c === a) {
